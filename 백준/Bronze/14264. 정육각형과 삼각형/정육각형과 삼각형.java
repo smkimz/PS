e196b2c030798b969e71caa05cb6b60b44495b44
smkimz <139411316+smkimz@users.noreply.github.com>
2024-06-28 22:24:02 +0900

    [Bronze III] Title: 정육각형과 삼각형, Time: 88 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		double L = Double.parseDouble(br.readLine());
+		System.out.println(Math.pow(L, 2) * Math.sqrt(3) / 4);
+	}
+}
