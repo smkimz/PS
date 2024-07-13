f6229ff93002a221cc1a2a09645cd06236591120
smkimz <139411316+smkimz@users.noreply.github.com>
2024-07-13 16:40:35 +0900

    [Silver II] Title: 아기 상어 2, Time: 336 ms, Memory: 100080 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -2,28 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 	static class Point {
-		int row, col, dist, num;
+		int row, col, dist;
 
-		public Point(int row, int col, int dist, int num) {
+		public Point(int row, int col, int dist) {
 			this.row = row;
 			this.col = col;
 			this.dist = dist;
-			this.num = num;
 		}
 	}
 
-	static final int INF = Integer.MAX_VALUE;
-	static int N, M, B = 0;
-	static int[] safeDistance;
+	static int N, M, ans = -1;
 	static boolean[][] map;
-	static boolean[][][] vt;
-	static Deque<Point> dq = new ArrayDeque<>();
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,53 +32,40 @@ public class Main {
 				map[i][j] = st.nextToken().equals("0") ? false : true;
 				if (map[i][j])
 					continue;
-				temp.add(new Point(i, j, 0, B));
-				++B;
+				temp.add(new Point(i, j, 0));
 			}
 		}
 
-		vt = new boolean[B][N][M];
 		Point p;
 		while (!temp.isEmpty()) {
 			p = temp.poll();
-			dq.add(p);
-			vt[p.num][p.row][p.col] = true;
+			checkMap(p.row, p.col);
 		}
-
-		safeDistance = new int[B];
-		Arrays.fill(safeDistance, INF);
-		checkMap();
-
-		int max = -1;
-		for (int i = 0; i < B; i++)
-			max = Math.max(max, safeDistance[i]);
-		System.out.println(max);
+		System.out.println(ans);
 	}
 
-	static void checkMap() {
+	static void checkMap(int r, int c) {
+		Deque<Point> dq = new ArrayDeque<>();
+		dq.add(new Point(r, c, 0));
+		boolean[][] vt = new boolean[N][M];
+		vt[r][c] = true;
 		int[][] dt = { { -1, 0, 1, 1, 1, 0, -1, -1 }, { 1, 1, 1, 0, -1, -1, -1, 0 } };
 		int dr, dc;
 		Point p;
 		while (!dq.isEmpty()) {
 			p = dq.poll();
-			if (safeDistance[p.num] != INF)
-				continue;
 			for (int i = 0; i <= 7; i++) {
 				dr = p.row + dt[0][i];
 				dc = p.col + dt[1][i];
-				if (isImmovable(dr, dc, p.num))
+				if (dr < 0 || dr > N - 1 || dc < 0 || dc > M - 1 || vt[dr][dc])
 					continue;
 				if (map[dr][dc]) {
-					safeDistance[p.num] = p.dist + 1;
-					continue;
+					ans = Math.max(ans, p.dist + 1);
+					return;
 				}
-				dq.add(new Point(dr, dc, p.dist + 1, p.num));
-				vt[p.num][dr][dc] = true;
+				dq.add(new Point(dr, dc, p.dist + 1));
+				vt[dr][dc] = true;
 			}
 		}
-	}
-
-	static boolean isImmovable(int r, int c, int n) {
-		return r < 0 || r > N - 1 || c < 0 || c > M - 1 || vt[n][r][c];
 	}
 }
