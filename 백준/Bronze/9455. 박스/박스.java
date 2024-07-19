724268954259b120b1b8fdf439b904fadedc54ca
smkimz <139411316+smkimz@users.noreply.github.com>
2024-07-19 21:05:52 +0900

    [Bronze I] Title: 박스, Time: 260 ms, Memory: 30376 KB -BaekjoonHub

---
+++ 백준/Bronze/9455. 박스/박스.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine()), N, M, sum;
+		int[][] cnt;
+		while (--T >= 0) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			sum = 0;
+			cnt = new int[M][2];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < M; j++) {
+					if (Integer.parseInt(st.nextToken()) == 0)
+						continue;
+					++cnt[j][0];
+					cnt[j][1] += N - i - 1;
+				}
+			}
+			for (int i = 0; i < M; i++)
+				sum += cnt[i][1] - --cnt[i][0] * (cnt[i][0] + 1) / 2;
+			sb.append(sum).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
