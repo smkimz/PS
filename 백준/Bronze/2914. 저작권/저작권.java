6d447407aa212c5b5824754a12441976afaa0a45
smkimz <139411316+smkimz@users.noreply.github.com>
2024-08-02 21:49:22 +0900

    [Bronze III] Title: 저작권, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int A = Integer.parseInt(st.nextToken()), I = Integer.parseInt(st.nextToken());
+		System.out.print((A - 1) * (I - 1) + I);
+	}
+}
