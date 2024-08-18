2a53a9fe736c3074eb1b0c42c9e3d539de3f3604
smkimz <139411316+smkimz@users.noreply.github.com>
2024-08-18 13:47:46 +0900

    [Silver II] Title: Young한 에너지는 부족하다, Time: 460 ms, Memory: 61944 KB -BaekjoonHub

---
+++ 백준/Silver/23561. Young한 에너지는 부족하다/Young한 에너지는 부족하다.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] energy = new int[3 * N];
+		for (int i = 0; i < 3 * N; i++)
+			energy[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(energy);
+		System.out.println(energy[2 * N - 1] - energy[N]);
+	}
+}
