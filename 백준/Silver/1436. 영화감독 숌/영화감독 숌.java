7ebd46a53dc6baac8183a4bbbd7ef634eb05f69a
smkimz <139411316+smkimz@users.noreply.github.com>
2024-08-25 13:25:08 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 158476 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), num = 665;
+		while (N > 0) {
+			++num;
+			if (String.valueOf(num).contains("666"))
+				--N;
+		}
+		System.out.println(num);
+	}
+}
