dc61e116e393c5cf6005391aa1e41c64748e6ec6
smkimz <139411316+smkimz@users.noreply.github.com>
2024-09-05 11:50:15 +0900

    [Silver I] Title: 팰린드롬 파티션, Time: 84 ms, Memory: 12764 KB -BaekjoonHub

---
+++ 백준/Silver/2705. 팰린드롬 파티션/팰린드롬 파티션.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()), N, max = 1;
+		int[] memo = new int[1001];
+		memo[1] = 1;
+		while (--T >= 0) {
+			N = Integer.parseInt(br.readLine());
+			if (memo[N] == 0) {
+				for (int i = max + 1; i <= N; i++) {
+					memo[i] = 1;
+					for (int j = 1; j <= i / 2; j++)
+						memo[i] += memo[j];
+				}
+			}
+			sb.append(memo[N]).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
