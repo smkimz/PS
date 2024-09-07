37b595f45e2d3b12807affc68b3187f16cc7cb64
smkimz <139411316+smkimz@users.noreply.github.com>
2024-09-07 11:17:37 +0900

    [Silver IV] Title: 좋은 단어, Time: 156 ms, Memory: 18796 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Deque<Character> stack = new ArrayDeque<>();
+		int N = Integer.parseInt(br.readLine()), ans = 0;
+		char[] str;
+		while (--N >= 0) {
+			str = br.readLine().toCharArray();
+			for (int i = 0; i < str.length; i++) {
+				if (stack.isEmpty() || str[i] != stack.peekLast())
+					stack.add(str[i]);
+				else
+					stack.pollLast();
+			}
+			ans += stack.isEmpty() ? 1 : 0;
+			stack.clear();
+		}
+		System.out.println(ans);
+	}
+}
