b5884b5300d9de39349bb167dd498a4f87ae9e13
smkimz <139411316+smkimz@users.noreply.github.com>
2024-09-09 16:42:21 +0900

    [Gold V] Title: a^b, Time: 68 ms, Memory: 11876 KB -BaekjoonHub

--- 백준/Gold/10827. a＾b/a＾b.java
+++ 백준/Gold/10827. a＾b/a＾b.java
@@ -9,10 +9,25 @@ public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		BigDecimal a = new BigDecimal(st.nextToken()), val = new BigDecimal(1.0);
-		int b = Integer.parseInt(st.nextToken());
-		while (--b >= 0)
+		BigDecimal a = new BigDecimal(st.nextToken()), val = a, ans = a;
+		int[][] pow = { {}, { 1, 1 }, { 1, 2, 4, 8, 16, 32, 64 }, { 1, 3, 9, 27, 81 } };
+		int b = Integer.parseInt(st.nextToken()), x = 1, y = 1, z = b;
+		for (int i = 2; i <= 3; i++) {
+			for (int j = 0; j < pow[i].length; j++) {
+				if (pow[i][j] < b || pow[x][y] < pow[i][j])
+					continue;
+				x = i;
+				y = j;
+			}
+		}
+
+		z -= pow[x][y];
+		for (int i = 1; i < x; i++)
 			val = val.multiply(a);
-		System.out.println(val.toPlainString());
+		for (int i = 1; i < y; i++)
+			ans = ans.multiply(val);
+		for (int i = 0; i < z; i++)
+			ans = ans.multiply(a);
+		System.out.println(ans.toPlainString());
 	}
 }
