111a9e47050a943591b855156d9b6db76a1defd4
smkimz <139411316+smkimz@users.noreply.github.com>
2024-09-14 15:41:11 +0900

    [Bronze II] Title: 자작나무가 없소~, Time: 368 ms, Memory: 79160 KB -BaekjoonHub

---
+++ 백준/Bronze/31496. 자작나무가 없소～/자작나무가 없소～.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), ans = 0, idxA, idxB, quantity;
+		char[] S = st.nextToken().toCharArray(), substr;
+		String itemName;
+		while (--N >= 0) {
+			st = new StringTokenizer(br.readLine());
+			itemName = st.nextToken();
+			quantity = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(itemName, "_");
+			p: while (st.hasMoreTokens()) {
+				substr = st.nextToken().toCharArray();
+				if (substr.length != S.length)
+					continue;
+				idxA = 0;
+				idxB = 0;
+				while (idxA < substr.length) {
+					if (substr[idxA++] != S[idxB])
+						break;
+					if (++idxB < S.length)
+						continue;
+					ans += quantity;
+					break p;
+				}
+			}
+		}
+		System.out.println(ans);
+	}
+}
