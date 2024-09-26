2212a1658c3cd270c43db1d9331b45b951336723
smkimz <139411316+smkimz@users.noreply.github.com>
2024-09-26 09:42:55 +0900

    [Silver I] Title: 센티와 마법의 뿅망치, Time: 308 ms, Memory: 27988 KB -BaekjoonHub

---
+++ 백준/Silver/19638. 센티와 마법의 뿅망치/센티와 마법의 뿅망치.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), H = Integer.parseInt(st.nextToken()),
+				T = Integer.parseInt(st.nextToken()), t = T;
+		PriorityQueue<Integer> pqueue = new PriorityQueue<>(Collections.reverseOrder());
+		while (--N >= 0)
+			pqueue.add(Integer.parseInt(br.readLine()));
+		while (pqueue.peek() >= H && --t >= 0) {
+			pqueue.add((int) Math.floor(pqueue.poll() / 2));
+			if (pqueue.peek() >= H)
+				continue;
+			if (pqueue.peek() <= 1)
+				pqueue.add(1);
+			break;
+		}
+		if (pqueue.peek() < H)
+			sb.append("YES\n").append(T - t);
+		else
+			sb.append("NO\n").append(pqueue.poll());
+		System.out.print(sb);
+	}
+}
