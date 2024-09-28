95bb1d6b11f49d89cdff661e5707ec6c51b0d5ac
smkimz <139411316+smkimz@users.noreply.github.com>
2024-09-28 10:11:44 +0900

    [Silver I] Title: 신입 사원, Time: 1508 ms, Memory: 301532 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,46 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static class Employee implements Comparable<Employee> {
+		int A, B;
+
+		public Employee(int a, int b) {
+			A = a;
+			B = b;
+		}
+
+		@Override
+		public int compareTo(Main.Employee o) {
+			if (this.A == o.A)
+				return Integer.compare(o.B, this.B);
+			return Integer.compare(this.A, o.A);
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine()), N, R, ans;
 		StringTokenizer st;
-		PriorityQueue<int[]> employees;
-		int[] employee;
+		PriorityQueue<Employee> employees;
+		Employee employee;
 		while (--T >= 0) {
 			N = Integer.parseInt(br.readLine());
 			R = 100000;
 			ans = 0;
-			employees = new PriorityQueue<>(new Comparator<int[]>() {
-				@Override
-				public int compare(int[] o1, int[] o2) {
-					if (o1[0] == o2[0])
-						return Integer.compare(o2[1], o1[1]);
-					return Integer.compare(o1[0], o2[0]);
-				}
-			});
-
+			employees = new PriorityQueue<>();
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				employees.add(new int[] { Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()) });
+				employees.add(new Employee(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 			}
-
 			while (--N >= 0) {
 				employee = employees.poll();
-				if (employee[1] > R)
+				if (employee.B > R)
 					continue;
 				++ans;
-				R = Math.min(R, employee[1]);
+				R = Math.min(R, employee.B);
 			}
-
 			sb.append(ans).append("\n");
 		}
 		System.out.print(sb);
