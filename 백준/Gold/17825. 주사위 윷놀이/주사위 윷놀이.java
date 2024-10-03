21135afc02291ae30546a21f466b94686fe9243d
smkimz <139411316+smkimz@users.noreply.github.com>
2024-10-03 18:10:18 +0900

    [Gold II] Title: 주사위 윷놀이, Time: 96 ms, Memory: 12700 KB -BaekjoonHub

--- 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
+++ 백준/Gold/17825. 주사위 윷놀이/주사위 윷놀이.java
@@ -2,84 +2,73 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+	static final int END = 32, MOVECOUNT = 10, MAXMOVE = 5, MEEPLE = 4;
 	static final int[] SCORES = new int[] { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38,
 			40, 13, 16, 19, 22, 24, 28, 27, 26, 25, 30, 35, 0 };
-	static final int END = 32;
-	static int[] move = new int[10];
-	static int[] position = new int[4];
+	static final int[][] ADJNODES = new int[][] { { 1, 2, 3, 4, 5 }, { 2, 3, 4, 5, 6 }, { 3, 4, 5, 6, 7 },
+			{ 4, 5, 6, 7, 8 }, { 5, 6, 7, 8, 9 }, { 21, 22, 23, 29, 30 }, { 7, 8, 9, 10, 11 }, { 8, 9, 10, 11, 12 },
+			{ 9, 10, 11, 12, 13 }, { 10, 11, 12, 13, 14 }, { 24, 25, 29, 30, 31 }, { 12, 13, 14, 15, 16 },
+			{ 13, 14, 15, 16, 17 }, { 14, 15, 16, 17, 18 }, { 15, 16, 17, 18, 19 }, { 26, 27, 28, 29, 30 },
+			{ 17, 18, 19, 20, 32 }, { 18, 19, 20, 32, 32 }, { 19, 20, 32, 32, 32 }, { 20, 32, 32, 32, 32 },
+			{ 32, 32, 32, 32, 32 }, { 22, 23, 29, 30, 31 }, { 23, 29, 30, 31, 20 }, { 29, 30, 31, 20, 32 },
+			{ 25, 29, 30, 31, 20 }, { 29, 30, 31, 20, 32 }, { 27, 28, 29, 30, 31 }, { 28, 29, 30, 31, 20 },
+			{ 29, 30, 31, 20, 32 }, { 30, 31, 20, 32, 32 }, { 31, 20, 32, 32, 32 }, { 20, 32, 32, 32, 32 } };
+	static int[] move = new int[MOVECOUNT];
+	static int[] position = new int[MEEPLE];
 	static int ans = 0;
 	static ArrayList<Integer>[] adjList;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		init();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < MOVECOUNT; i++)
 			move[i] = Integer.parseInt(st.nextToken());
+		init();
 		checkMovement(0, 0);
 		System.out.println(ans);
 	}
 
-	static void init() {
-		adjList = new ArrayList[END + 1];
-		for (int i = 0; i <= END; i++)
+	private static void init() {
+		adjList = new ArrayList[END];
+		for (int i = 0; i < END; i++) {
 			adjList[i] = new ArrayList<Integer>();
-		for (int i = 0; i <= 19; i++)
-			adjList[i].add(i + 1);
-		adjList[5].add(21);
-		adjList[21].add(22);
-		adjList[22].add(23);
-		adjList[23].add(29);
-		adjList[10].add(24);
-		adjList[24].add(25);
-		adjList[25].add(29);
-		adjList[15].add(26);
-		adjList[26].add(27);
-		adjList[27].add(28);
-		adjList[28].add(29);
-		adjList[29].add(30);
-		adjList[30].add(31);
-		adjList[31].add(20);
-		adjList[20].add(END);
+			for (int j = 0; j < MAXMOVE; j++)
+				adjList[i].add(ADJNODES[i][j]);
+		}
 	}
 
-	static void checkMovement(int turn, int score) {
-		if (turn == 10) {
+	private static void checkMovement(int turn, int score) {
+		if (turn == MOVECOUNT) {
 			ans = Math.max(ans, score);
 			return;
 		}
-		int m, p, q;
-		p: for (int i = 0; i <= 3; i++) {
+		int pos, prevPos;
+		for (int i = 0; i < MEEPLE; i++) {
 			if (position[i] == END)
 				continue;
-			p = position[i];
-			m = move[turn];
-			if (p == 5 || p == 10 || p == 15) {
-				p = adjList[p].get(1);
-				--m;
-			}
-			while (m > 0 && p < END) {
-				p = adjList[p].get(0);
-				--m;
-			}
-			if (p < END) {
-				for (int j = 0; j <= 3; j++) {
-					if (i == j)
-						continue;
-					if (p == position[j]) {
-						continue p;
-					}
-				}
-			}
-			q = position[i];
-			position[i] = p;
+			pos = adjList[position[i]].get(move[turn] - 1);
+			if (isSameNode(i, pos))
+				continue;
+			prevPos = position[i];
+			position[i] = pos;
 			checkMovement(turn + 1, score + SCORES[position[i]]);
-			position[i] = q;
+			position[i] = prevPos;
 		}
+	}
+
+	private static boolean isSameNode(int m, int pos) {
+		if (pos == END)
+			return false;
+		for (int i = 0; i < MEEPLE; i++) {
+			if (m == i)
+				continue;
+			if (pos == position[i])
+				return true;
+		}
+		return false;
 	}
 }
