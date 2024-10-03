d63cc0cca16c55838f5ca86542127e8548715cac
smkimz <139411316+smkimz@users.noreply.github.com>
2024-10-03 20:32:17 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 56 ms, Memory: 11236 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		System.out.println(sb.append(2024).append("\n").append(10).append("\n").append(3));
+	}
+}
