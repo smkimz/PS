a0ba6a4655acb2e598c8c4c6f596d7c75bfe6ce0
smkimz <139411316+smkimz@users.noreply.github.com>
2024-10-04 15:58:26 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 68 ms, Memory: 11808 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -5,28 +5,25 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine()), idx = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] arr = new int[N], dp = new int[N];
 		for (int i = 0; i < N; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
 		dp[0] = arr[0];
-		int idx = 0;
 		for (int i = 1; i < N; i++) {
 			if (dp[idx] > arr[i])
 				dp[++idx] = arr[i];
 			else
-				dp[searchIndex(dp, arr[i])] = arr[i];
+				dp[searchIndex(dp, arr[i], idx)] = arr[i];
 		}
 		System.out.println(idx + 1);
 	}
 
-	static int searchIndex(int[] arr, int val) {
-		int start = 0, end = N - 1;
+	static int searchIndex(int[] arr, int val, int end) {
+		int start = 0;
 		while (start <= end) {
 			int mid = (start + end) / 2;
 			if (arr[mid] < val)
