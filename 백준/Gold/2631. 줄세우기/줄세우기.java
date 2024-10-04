e6a1c454909060c6866ce2e81bdbc273f2e88a02
smkimz <139411316+smkimz@users.noreply.github.com>
2024-10-04 16:17:28 +0900

    [Gold IV] Title: 줄세우기, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static final int MAX = 201;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), idx = 0;
+		int[] arr = new int[N], dp = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			dp[i] = MAX;
+		}
+		dp[0] = arr[0];
+		for (int i = 1; i < N; i++) {
+			if (dp[idx] < arr[i])
+				dp[++idx] = arr[i];
+			else
+				dp[searchIndex(dp, arr[i], idx)] = arr[i];
+		}
+		System.out.println(N - idx - 1);
+	}
+
+	static int searchIndex(int[] dp, int val, int end) {
+		int start = 0;
+		while (start <= end) {
+			int mid = (start + end) / 2;
+			if (dp[mid] > val)
+				end = mid - 1;
+			else if (dp[mid] < val)
+				start = mid + 1;
+			else
+				return mid;
+		}
+		return start;
+	}
+}
