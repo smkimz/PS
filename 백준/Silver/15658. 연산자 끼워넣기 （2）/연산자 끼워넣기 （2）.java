93ad5f9fe9672fa50686eef093a0a4e3c7c2a82f
smkimz <139411316+smkimz@users.noreply.github.com>
2024-10-30 09:47:41 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 88 ms, Memory: 13696 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] nums, signCount = new int[4], usedSignCount = new int[4];
+	static int N, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+	static char[] signs;
+
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		nums = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++)
+			nums[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < 4; i++)
+			signCount[i] = Integer.parseInt(st.nextToken());
+		backTracking(nums[0], 1);
+		System.out.println(sb.append(max).append("\n").append(min));
+	}
+
+	static void backTracking(int val, int idx) {
+		if (idx == N) {
+			max = Math.max(max, val);
+			min = Math.min(min, val);
+			return;
+		}
+		for (int i = 0; i <= 3; i++) {
+			if (usedSignCount[i] >= signCount[i])
+				continue;
+			++usedSignCount[i];
+			switch (i) {
+			case 0:
+				backTracking(val + nums[idx], idx + 1);
+				break;
+			case 1:
+				backTracking(val - nums[idx], idx + 1);
+				break;
+			case 2:
+				backTracking(val * nums[idx], idx + 1);
+				break;
+			case 3:
+				backTracking(val / nums[idx], idx + 1);
+				break;
+			default:
+				break;
+			}
+			--usedSignCount[i];
+		}
+	}
+}
