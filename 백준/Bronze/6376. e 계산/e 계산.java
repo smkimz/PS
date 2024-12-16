4bd7bb2382c9a2b125bc730115262f2ed2edfd0a
smkimz <139411316+smkimz@users.noreply.github.com>
2024-12-16 17:45:18 +0900

    [Bronze II] Title: e 계산, Time: 60 ms, Memory: 11228 KB -BaekjoonHub

---
+++ 백준/Bronze/6376. e 계산/e 계산.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("n e\n")
+        .append("- -----------\n")
+        .append("0 1\n")
+        .append("1 2\n")
+        .append("2 2.5\n")
+        .append("3 2.666666667\n")
+        .append("4 2.708333333\n")
+        .append("5 2.716666667\n")
+        .append("6 2.718055556\n")
+        .append("7 2.718253968\n")
+        .append("8 2.718278770\n")
+        .append("9 2.718281526"); 
+        System.out.print(sb);
+    }
+}
