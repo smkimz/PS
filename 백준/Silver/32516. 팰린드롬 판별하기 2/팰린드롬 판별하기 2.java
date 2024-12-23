0295a5ee30aaf34c7ade95b57a59598a82a0707c
smkimz <139411316+smkimz@users.noreply.github.com>
2024-12-23 15:47:25 +0900

    [Silver IV] Title: 팰린드롬 판별하기 2, Time: 100 ms, Memory: 15480 KB -BaekjoonHub

--- 백준/Silver/32516. 팰린드롬 판별하기 2/팰린드롬 판별하기 2.java
+++ 백준/Silver/32516. 팰린드롬 판별하기 2/팰린드롬 판별하기 2.java
@@ -1,14 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()), r, ans = 0;
+		int N = Integer.parseInt(br.readLine()), r, ans = 0;
 		char[] T = br.readLine().toCharArray();
 		for (int i = 0; i < N / 2; i++) {
 			r = N - i - 1;
