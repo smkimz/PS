f33bcd1405abe03b6602fb58c848b168a441022d
smkimz <139411316+smkimz@users.noreply.github.com>
2025-01-19 13:10:35 +0900

    [Bronze I] Title: 불안정한 수열, Time: 236 ms, Memory: 34116 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Bronze/28323. 불안정한 수열/불안정한 수열.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()) - 1, x, y, ans = 1;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		x = Integer.parseInt(st.nextToken());
+		while (--N >= 0) {
+			y = Integer.parseInt(st.nextToken());
+			if (x % 2 == y % 2)
+				continue;
+			x = y;
+			++ans;
+		}
+		if (ans == 1 && x % 2 == 0)
+			System.out.println(0);
+		else
+			System.out.println(ans);
+	}
+}
