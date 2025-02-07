21e0060f9eaa3378e59749311927b7d703000cef
smkimz <139411316+smkimz@users.noreply.github.com>
2025-02-07 14:49:54 +0900

    [Bronze III] Title: 완전 제곱수, Time: 68 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -6,8 +6,14 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()), diff, ans = 0;
+		if (N == 0) {
+			System.out.println(N);
+			return;
+		}
 		for (int i = 1; i <= 500; i++) {
-			for (int j = i; j <= 500; j++) {
+			if (Math.pow(i + 1, 2) - Math.pow(i, 2) > N)
+				break;
+			for (int j = i + 1; j <= 500; j++) {
 				diff = j * j - i * i;
 				if (diff < N)
 					continue;
