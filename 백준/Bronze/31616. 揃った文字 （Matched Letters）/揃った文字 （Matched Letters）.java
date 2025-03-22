157ace9d13d2bc436049d9f761e5266048012ec8
smkimz <139411316+smkimz@users.noreply.github.com>
2025-03-22 13:54:03 +0900

    [Bronze IV] Title: 揃った文字 (Matched Letters), Time: 68 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/31616. 揃った文字 （Matched Letters）/揃った文字 （Matched Letters）.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		char[] S = br.readLine().toCharArray();
+		for(int i = 1; i < N; i++) {
+			if(S[i] != S[i-1]) {
+				System.out.println("No");
+				return;
+			}
+		}
+		System.out.println("Yes");
+	}
+}
