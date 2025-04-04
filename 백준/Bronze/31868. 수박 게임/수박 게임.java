db86d1714c0fbf397b5f4ec6ad44eb09c717c834
smkimz <139411316+smkimz@users.noreply.github.com>
2025-04-04 15:37:15 +0900

    [Bronze III] Title: 수박 게임, Time: 68 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/31868. 수박 게임/수박 게임.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken());
+		System.out.println(K / (int) Math.pow(2, N - 1));
+	}
+}
