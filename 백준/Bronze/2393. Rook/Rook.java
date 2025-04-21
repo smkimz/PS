f18828fcee3f41377807302080603f8ff18d39f7
smkimz <139411316+smkimz@users.noreply.github.com>
2025-04-21 14:43:19 +0900

    [Bronze V] Title: Rook, Time: 60 ms, Memory: 11344 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder("  ___  ___  ___").append("\n")
+				.append("  | |__| |__| |").append("\n")
+				.append("  |           |").append("\n")
+				.append("   \\_________/").append("\n")
+				.append("    \\_______/").append("\n")
+				.append("     |     |").append("\n")
+				.append("     |     |").append("\n")
+				.append("     |     |").append("\n")
+				.append("     |     |").append("\n")
+				.append("     |_____|").append("\n")
+				.append("  __/       \\__").append("\n")
+				.append(" /             \\").append("\n")
+				.append("/_______________\\");
+		System.out.print(sb);
+	}
+}
