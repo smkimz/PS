e8126ad3882d14e6dd53cccb9aa08038e29330e8
smkimz <139411316+smkimz@users.noreply.github.com>
2025-05-25 11:21:02 +0900

    [Bronze III] Title: 스케이트보드, Time: 404 ms, Memory: 64312 KB -BaekjoonHub

--- 백준/Bronze/28417. 스케이트보드/스케이트보드.java
+++ 백준/Bronze/28417. 스케이트보드/스케이트보드.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine()), score, ans = 0;
+		int[] scores = new int[5];
 		StringTokenizer st;
-		PriorityQueue<Integer> pq;
 		while (--N >= 0) {
 			st = new StringTokenizer(br.readLine());
 			score = 0;
-			pq = new PriorityQueue<Integer>(Collections.reverseOrder());
 			for (int i = 0; i < 2; i++)
 				score = Math.max(score, Integer.parseInt(st.nextToken()));
 			for (int i = 0; i < 5; i++)
-				pq.add(Integer.parseInt(st.nextToken()));
-			score += pq.poll() + pq.poll();
+				scores[i] = Integer.parseInt(st.nextToken());
+			Arrays.sort(scores);
+			score += scores[3] + scores[4];
 			ans = Math.max(ans, score);
 		}
 		System.out.println(ans);
