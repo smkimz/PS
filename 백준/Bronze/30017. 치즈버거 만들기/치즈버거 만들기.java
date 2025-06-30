8efed7db013b38539bf2d5d3ec095391b53d51e5
smkimz <139411316+smkimz@users.noreply.github.com>
2025-06-30 18:04:12 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 68 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+		System.out.println(a > b ? b * 2 + 1 : a * 2 - 1);
+	}
+}
