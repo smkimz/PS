acff7c42c6798ad691aba75ae4079f56ea13cd12
smkimz <139411316+smkimz@users.noreply.github.com>
2025-08-08 20:35:25 +0900

    [Bronze IV] Title: DORO, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

---
+++ 백준/Bronze/34073. DORO/DORO.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		while (--N >= 0)
+			sb.append(st.nextToken()).append("DORO ");
+		System.out.print(sb);
+	}
+}
