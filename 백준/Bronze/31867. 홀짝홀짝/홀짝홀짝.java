05b224b71bb17f76ffdf35f5a09559f0edbf3d18
smkimz <139411316+smkimz@users.noreply.github.com>
2025-08-13 20:43:03 +0900

    [Bronze IV] Title: 홀짝홀짝, Time: 156 ms, Memory: 22236 KB -BaekjoonHub

---
+++ 백준/Bronze/31867. 홀짝홀짝/홀짝홀짝.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()), r, cnt = 0;
+		char[] str = br.readLine().toCharArray();
+		for (int i = 0; i < N; i++) {
+			r = (str[i] - '0') % 2;
+			if (r == 1)
+				--cnt;
+			else
+				++cnt;
+		}
+		if (cnt == 0)
+			System.out.println(-1);
+		else
+			System.out.println(cnt > 0 ? 0 : 1);
+	}
+}
