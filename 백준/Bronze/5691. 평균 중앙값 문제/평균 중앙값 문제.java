1ab567991c48b5748e5e549984f35bd9238cffe6
smkimz <139411316+smkimz@users.noreply.github.com>
2025-08-15 12:21:17 +0900

    [Bronze III] Title: 평균 중앙값 문제, Time: 64 ms, Memory: 11360 KB -BaekjoonHub

---
+++ 백준/Bronze/5691. 평균 중앙값 문제/평균 중앙값 문제.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int a, b;
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			if (a == 0)
+				break;
+			b = Integer.parseInt(st.nextToken());
+			sb.append(2 * a - b).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
