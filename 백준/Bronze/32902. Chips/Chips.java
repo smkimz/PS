3d2ed98ac74da95d419c0291503a5489e3e0ec3d
smkimz <139411316+smkimz@users.noreply.github.com>
2025-08-27 17:00:44 +0900

    [Bronze IV] Title: Chips, Time: 64 ms, Memory: 11672 KB -BaekjoonHub

---
+++ 백준/Bronze/32902. Chips/Chips.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int k = Integer.parseInt(st.nextToken()), n = Integer.parseInt(st.nextToken());
+		System.out.printf("%d %d", n+1, k * n + 1);
+	}
+}
