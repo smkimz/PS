db3d1af9a1cef356bda36b72181dce756bf916cf
smkimz <139411316+smkimz@users.noreply.github.com>
2025-09-29 17:54:40 +0900

    [Bronze V] Title: 세제곱의 합, Time: 68 ms, Memory: 11792 KB -BaekjoonHub

--- 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.java
@@ -6,11 +6,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine()), s = 0, t = 0;
-		for (int i = 1; i <= N; i++) {
-			s += i;
-			t += Math.pow(i, 3);
-		}
-		System.out.printf("%d\n%d\n%d", s, (int) Math.pow(s, 2), t);
+		int N = Integer.parseInt(br.readLine()), s = N * (N + 1) / 2, t = (int) Math.pow(s, 2);
+		System.out.printf("%d\n%d\n%d", s, t, t);
 	}
 }
