585a0d6ae5496f33a820197ed70df961f8f83233
smkimz <139411316+smkimz@users.noreply.github.com>
2025-10-28 16:37:47 +0900

    [Bronze III] Title: 갈래의 색종이 자르기, Time: 68 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/31472. 갈래의 색종이 자르기/갈래의 색종이 자르기.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int W = Integer.parseInt(br.readLine());
+		System.out.println((int) Math.sqrt(W * 2) * 4);
+	}
+}
