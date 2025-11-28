034f7b013d11f4f66793f7f1055a93f600337d98
smkimz <139411316+smkimz@users.noreply.github.com>
2025-11-28 17:15:10 +0900

    [Bronze II] Title: 랜덤 숫자 만들기, Time: 384 ms, Memory: 119752 KB -BaekjoonHub

--- 백준/Bronze/6500. 랜덤 숫자 만들기/랜덤 숫자 만들기.java
+++ 백준/Bronze/6500. 랜덤 숫자 만들기/랜덤 숫자 만들기.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int a, b;
+		int a, b, ret;
 		int[] P = new int[] { 100, 1000, 10000, 100000 };
-		HashSet<Integer> rdnums;
+		boolean[] rdnums;
 		while (true) {
 			a = Integer.parseInt(br.readLine());
 			if (a == 0)
 				break;
-			rdnums = new HashSet<Integer>();
-			while (!rdnums.contains(a)) {
-				rdnums.add(a);
+			rdnums = new boolean[10000];
+			ret = 0;
+			while (!rdnums[a]) {
+				rdnums[a] = true;
+				++ret;
 				a = (int) Math.pow(a, 2);
 				b = 0;
 				for (int i = 3; i >= 0; i--) {
@@ -26,7 +27,7 @@ public class Main {
 				}
 				a = b;
 			}
-			sb.append(rdnums.size()).append("\n");
+			sb.append(ret).append("\n");
 		}
 		System.out.print(sb);
 	}
