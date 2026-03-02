fd1c71714e868b5b2dfba21a5595945a519fd3a2
smkimz <139411316+smkimz@users.noreply.github.com>
2026-03-02 18:50:51 +0900

    [Bronze IV] Title: YCPC 점수, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/34823. YCPC 점수/YCPC 점수.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] x = new int[3];
+		for (int i = 0; i < 3; i++)
+			x[i] = Integer.parseInt(st.nextToken());
+		System.out.println(Math.min(Math.min(x[0], x[2]), x[1] / 2));
+	}
+}
