ab213d469d3976b7b8c44a77f9bbc3f3da395cfd
smkimz <139411316+smkimz@users.noreply.github.com>
2026-03-06 17:14:25 +0900

    [Bronze IV] Title: FYI, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()) / 10000;
+		System.out.println(N == 555 ? "YES" : "NO");
+	}
+}
