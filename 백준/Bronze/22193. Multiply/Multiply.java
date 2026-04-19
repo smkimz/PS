bb4bc7e9002c5ab43a4018bf99ad3cb966f14865
smkimz <139411316+smkimz@users.noreply.github.com>
2026-04-19 20:36:50 +0900

    [Bronze V] Title: Multiply, Time: 512 ms, Memory: 53724 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/22193. Multiply/Multiply.java
+++ 백준/Bronze/22193. Multiply/Multiply.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		br.readLine();
-		int N = Integer.parseInt(br.readLine()), M = Integer.parseInt(br.readLine());
-		System.out.println(N * M);
+		BigInteger N = new BigInteger(br.readLine()), M = new BigInteger(br.readLine());
+		System.out.println(N.multiply(M));
 	}
 }
